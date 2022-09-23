b7a3cff26448f9456803d802a2b12847d34e3af8
oshikombo <amukuhupolykarp@gmail.com>
2022-09-23 03:18:33 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,15 +2,16 @@
 
 /**
  * rot13 - encodes a string into rot13
-  * @s: string to encode
-  *
-  * Return: address of s
-  */
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
 char *rot13(char *s)
 {
 	int i, j;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j < 52; j++)
