ed32118a729bba19df08d9a85391d9b7641083d6
oshikombo <amukuhupolykarp@gmail.com>
2022-09-26 14:11:48 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
- * _memcpy - copies memory area
- * @dest:char
- * @src:char
- * @n:int
- * Return:char pointer
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to the memory block
  */
+
 char *_memcpy(char *s, char *b, unsigned int n)
 {
 	while (n)
