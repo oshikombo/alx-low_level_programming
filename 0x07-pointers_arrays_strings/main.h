d49d1f3095859c010bfe6b1793b786f148e85309
oshikombo <amukuhupolykarp@gmail.com>
2022-09-26 14:37:49 -0700

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * void prototypes()
- * int prototypes()
-*/
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -17,4 +9,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_H*/
