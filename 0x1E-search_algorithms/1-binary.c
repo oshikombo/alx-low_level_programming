7943477072ee9b0cfbfcd73525fc007b52291227
oshikombo <amukuhupolykarp@gmail.com>
2023-04-13 14:11:07 -0700

    Task 1,2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,13 @@
-#include "search_algos.h"
+var sorted = require('sorted-array-functions')
+var list = []
 
-/**
- * binary_search - searches for a value in an integer array using a binary
- * search algorithm, not guaranteed to return lowest index if `value` appears
- * twice in `array`
- * @array: pointer to first element of array to seach
- * @size: number of elements in array
- * @value: value to search for
- *
- * Return: index containing `value`, or -1 if `value` not found or
- * `array` is NULL
- */
+sorted.add(list, 1)
+sorted.add(list, 4)
+sorted.add(list, 2)
 
-int binary_search(int *array, size_t size, int value)
-{
-	size_t left, right, mid, i;
-
-	if (!array)
-		return (-1);
-
-	left = 0;
-	right = size - 1;
-
-	while (left <= right)
-	{
-		mid = (left + right) / 2;
-		printf("Searching in array: ");
-		for (i = left; i <= right; i++)
-			printf("%i%s", array[i], i == right ? "\n" : ", ");
-		if (array[mid] < value)
-			left = mid + 1;
-		else if (array[mid] > value)
-			right = mid - 1;
-		else
-			return ((int)mid);
-	}
-
-	return (-1);
-}
+console.log(list) // prints out [1, 2, 4]
+console.log(sorted.has(list, 2)) // returns true
+console.log(sorted.has(list, 3)) // returns false
+console.log(sorted.eq(list, 2)) // returns 1 (the index)
+console.log(sorted.gt(list, 2)) // returns 2
+console.log(sorted.gt(list, 4)) // returns -1
